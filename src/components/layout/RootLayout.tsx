7b03a383b8e5192992b44a46ca6333b840d23fc1
Freddy Orozco <freddycamilo95@gmail.com>
2026-02-22 17:13:33 -0500

    feat-page-about: Add first version of the AboutMe page

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,6 +1,7 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 
 import { Footer } from './Footer';
+import { Logo } from './Logo';
 import { Navbar } from './Navbar';
 
 type NavbarActiveItem = 'about' | 'projects' | 'contact' | null;
@@ -18,9 +19,14 @@ export function RootLayout() {
   return (
     <div className="min-h-screen flex flex-col">
       <div className="flex flex-1">
-        <main className="flex-1">
-          <Outlet />
-        </main>
+        <div className="flex flex-col flex-1">
+          <Link to="/" className="px-5 pt-5 inline-block self-start" aria-label="Go to home">
+            <Logo />
+          </Link>
+          <main className="flex-1">
+            <Outlet />
+          </main>
+        </div>
         <aside>
           <Navbar activeItem={getActiveItem(pathname)} />
         </aside>
