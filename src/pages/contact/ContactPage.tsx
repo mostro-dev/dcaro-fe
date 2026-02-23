37d4f84cf35f6c832a286e628d8810c5b4d51429
Freddy Orozco <freddycamilo95@gmail.com>
2026-02-23 18:53:19 -0500

    feat-page-about: Update Pages to not use old root components

--- src/pages/contact/ContactPage.tsx
+++ src/pages/contact/ContactPage.tsx
@@ -1,7 +1,28 @@
+import { Link } from 'react-router-dom';
+
+import { Footer } from '../../components/layout/Footer';
+import { LogoTopBar } from '../../components/layout/LogoTopBar';
+import { Navbar } from '../../components/layout/Navbar';
+
 export default function ContactPage() {
   return (
-    <div>
-      <h1>Contact page works!</h1>
+    <div className="min-h-screen flex flex-col">
+      <div className="flex flex-1">
+        <div className="flex flex-col flex-1">
+          <Link to="/" className="px-5 pt-5 inline-block self-start" aria-label="Go to home">
+            <LogoTopBar />
+          </Link>
+          <main className="flex-1">
+            <div>
+              <h1>Contact page works!</h1>
+            </div>
+          </main>
+        </div>
+        <aside>
+          <Navbar activeItem="contact" />
+        </aside>
+      </div>
+      <Footer />
     </div>
   );
 }
