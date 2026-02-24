e7c40529bfbe2038c45ed63848b0cefd87e45776
Freddy Orozco <freddycamilo95@gmail.com>
2026-02-24 11:15:09 -0500

    feat-mix-misc-sizes: Update icon and tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,17 +22,17 @@ const config: Config = {
       // Font sizes from Figma Design System (with lineHeight 1 = 100%)
       fontSize: {
         // Caro_H1: Montserrat Medium, 20px
-        'h1': ['20px', { lineHeight: '1', letterSpacing: '0', fontWeight: '500' }],
+        'h1': ['20px', { lineHeight: '1.5', letterSpacing: '0', fontWeight: '500' }],
         // Caro_H2: Montserrat Medium, 16px
-        'h2': ['16px', { lineHeight: '1', letterSpacing: '0', fontWeight: '500' }],
+        'h2': ['16px', { lineHeight: '1.5', letterSpacing: '0', fontWeight: '500' }],
         // Caro_H3: Montserrat Regular, 15px
-        'h3': ['15px', { lineHeight: '1', letterSpacing: '0', fontWeight: '400' }],
+        'h3': ['15px', { lineHeight: '1.5', letterSpacing: '0', fontWeight: '400' }],
         // Caro_Regular: Montserrat Regular, 15px
-        'regular': ['15px', { lineHeight: '1', letterSpacing: '0', fontWeight: '400' }],
+        'regular': ['15px', { lineHeight: '1.5', letterSpacing: '0', fontWeight: '400' }],
         // Caro_Small: Jura Regular, 11px
-        'small': ['11px', { lineHeight: '1', letterSpacing: '0', fontWeight: '400' }],
+        'small': ['12px', { lineHeight: '1.5', letterSpacing: '0', fontWeight: '400' }],
         // Caro_Xsmall: Jura Regular, 7px
-        'xsmall': ['7px', { lineHeight: '1', letterSpacing: '0', fontWeight: '400' }],
+        'xsmall': ['10px', { lineHeight: '1.5', letterSpacing: '0', fontWeight: '400' }],
       },
     },
   },
