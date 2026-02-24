7dc3d3d3093b77b1293f04faf87436d369596539
Freddy Orozco <freddycamilo95@gmail.com>
2026-02-24 09:39:25 -0500

    feat-base-pages: Fix pages to follow same style

--- src/pages/contact/ContactPage.tsx
+++ src/pages/contact/ContactPage.tsx
@@ -1,28 +1,50 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 import { Footer } from '../../components/layout/Footer';
-import { LogoTopBar } from '../../components/layout/LogoTopBar';
-import { Navbar } from '../../components/layout/Navbar';
 
 export default function ContactPage() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMounted(true);
+    }, 250);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="flex flex-1">
-        <div className="flex flex-col flex-1">
-          <Link to="/" className="px-5 pt-5 inline-block self-start" aria-label="Go to home">
-            <LogoTopBar />
-          </Link>
-          <main className="flex-1">
-            <div>
-              <h1>Contact page works!</h1>
-            </div>
-          </main>
+    <>
+      <main className="flex-1">
+        <div className="px-5 pt-8 flex flex-col gap-6">
+          <h1
+            className={`font-montserrat text-h1 font-medium text-black transition-all duration-500 ease-out ${
+              mounted ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'
+            }`}
+          >
+            Contact me!
+          </h1>
+          <div
+            className={`transition-all duration-1000 ease-out delay-200 ${
+              mounted ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'
+            }`}
+          >
+            <p className="hidden font-montserrat text-regular text-black mb-4">
+              Send me an email at{' '}
+              <a href="mailto:contacto@dcaro.co" className="text-red">
+                contacto@dcaro.co
+              </a>
+              .
+            </p>
+            <p className="font-montserrat text-regular text-black">
+              Follow me on my{' '}
+              <a href="https://www.instagram.com/dcaro.co/" className="text-red" target="_blank">
+                Instagram account!
+              </a>
+            </p>
+          </div>
         </div>
-        <aside>
-          <Navbar activeItem="contact" />
-        </aside>
-      </div>
+      </main>
       <Footer />
-    </div>
+    </>
   );
 }
